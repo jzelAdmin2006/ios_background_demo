3f4e63a2e1874667401a11ab6c0a7c9f50225de5
jzelAdmin2006 <jo.zeller@protonmail.com>
2024-10-15 15:41:02 -0700

    try fixing iOS background notifications

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,27 @@
-import Flutter
 import UIKit
 
-@main
+import Flutter
+
+import flutter_local_notifications
+
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+      FlutterLocalNotificationsPlugin.setPluginRegistrantCallback{ (registry) in GeneratedPluginRegistrant.register(with: registry) }
+
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+    UNUserNotificationCenter.current().delegate = self
+
+    return true
   }
+
+    override func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound, .badge])
+    }
+
 }
